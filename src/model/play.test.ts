d4cd235fab0c5c061d3fb40234706ea4ab08a5dc
Rasmus Schultz <rasmus@mindplay.dk>
2025-08-08 19:12:24 +0200

    implement game rules and logic

--- src/model/play.test.ts
+++ src/model/play.test.ts
@@ -6,30 +6,296 @@ import { PlayerNumber, Player, Player1, Player2 } from "./players"
 import { Stone, StoneType, FlatStone, StandingStone, CapStone } from "./stones"
 import { Turn, Place, Move, Direction, Up, Down, Left, Right } from "./turns"
 
-test("can place stones", () => {
-  let game = createNewGame(3)
-
-  game = play(game, Place(0, 0)) // P1
-
-  expect(game.player).toBe(2)
-
-  expect(game.reserve).toEqual({
-    1: { stones: 9, capstones: 0 },
-    2: { stones: 10, capstones: 0 },
+describe("Rule 1.1: Board Setup", () => {
+  test("Rule 1.1: cannot place out of bounds", () => {
+    let game = createNewGame(3)
+    
+    expect(() => play(game, Place(3, 0))).toThrow(
+      "Position out of bounds: rank 3, file 0 on a 3x3 board"
+    )
+    
+    expect(() => play(game, Place(0, 3))).toThrow(
+      "Position out of bounds: rank 0, file 3 on a 3x3 board"
+    )
+    
+    expect(() => play(game, Place(-1, 0))).toThrow(
+      "Position out of bounds: rank -1, file 0 on a 3x3 board"
+    )
   })
 
-  game = play(game, Place(1, 1)) // P2
+  test("Rule 1.1: cannot place stone when no stones in reserve", () => {
+    let game = createNewGame(3)
 
-  expect(game.player).toBe(1)
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
 
-  expect(game.reserve).toEqual({
-    1: { stones: 9, capstones: 0 },
-    2: { stones: 9, capstones: 0 },
+    for (let n=0; n<9; n++) {
+      game = play(game, Place(1, 0)) // P1
+      game = play(game, Place(1, 1)) // P2
+      game = play(game, Move(1, 0, Up, [1])) // P1
+      game = play(game, Move(1, 1, Up, [1])) // P1
+    }
+    
+    expect(() => play(game, Place(1, 0))).toThrow(
+      "Player 1 does not have stones in reserve"
+    )
   })
 
-  expect(game.board.squares).toEqual([
-    [[Stone(1)], [        ], [        ]],
-    [[        ], [Stone(2)], [        ]],
-    [[        ], [        ], [        ]],
-  ])
+  test("Rule 1.1: cannot place capstone when no capstones in reserve", () => {
+    let game = createNewGame(3) // there are no capstones in a 3x3 game
+
+    game = play(game, Place(0, 0))
+    game = play(game, Place(0, 1))
+    
+    expect(() => play(game, Place(0, 2, CapStone))).toThrow(
+      "Player 1 does not have capstones in reserve"
+    )
+  })
+})
+
+describe("Rule 1.2: First two turns", () => {
+  test("turn 1 must place flat stone", () => {
+    let game = createNewGame(5)
+    
+    expect(() => play(game, Place(0, 0, StandingStone))).toThrow(
+      "Turn 1 must place a flat stone"
+    )
+    
+    expect(() => play(game, Place(0, 0, CapStone))).toThrow(
+      "Turn 1 must place a flat stone"
+    )
+  })
+
+  test("turn 2 must place flat stone", () => {
+    let game = createNewGame(5)
+
+    game = play(game, Place(0, 0, FlatStone)) // P1
+    
+    expect(() => play(game, Place(1, 1, StandingStone))).toThrow(
+      "Turn 2 must place a flat stone"
+    )
+    
+    expect(() => play(game, Place(1, 1, CapStone))).toThrow(
+      "Turn 2 must place a flat stone"
+    )
+  })
+})
+
+describe("Rule 2.1: Placing Stones", () => {
+  test("can place stones", () => {
+    let game = createNewGame(3)
+
+    game = play(game, Place(0, 0)) // P1
+
+    expect(game.player).toBe(2)
+
+    expect(game.reserve).toEqual({
+      1: { stones: 9, capstones: 0 },
+      2: { stones: 10, capstones: 0 },
+    })
+
+    game = play(game, Place(1, 1)) // P2
+
+    expect(game.player).toBe(1)
+
+    expect(game.reserve).toEqual({
+      1: { stones: 9, capstones: 0 },
+      2: { stones: 9, capstones: 0 },
+    })
+
+    expect(game.board.squares).toEqual([
+      [[Stone(1)], [        ], [        ]],
+      [[        ], [Stone(2)], [        ]],
+      [[        ], [        ], [        ]],
+    ])
+  })
+})
+
+describe("Rule 2.2: Moving Stones", () => {
+  test("can move stones up", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(1, 1)) // P1
+    game = play(game, Place(1, 2)) // P2
+    game = play(game, Move(1, 1, Up, [1])) // P1
+    
+    expect(game.board.squares).toEqual([
+      [[], [Stone(1)], [        ]],
+      [[], [        ], [Stone(2)]],
+      [[], [        ], [        ]],
+    ])
+  })
+
+  test("can move stones down", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(1, 1)) // P1
+    game = play(game, Place(1, 2)) // P2
+    game = play(game, Move(1, 1, Down, [1])) // P1
+    
+    expect(game.board.squares).toEqual([
+      [[], [        ], [        ]],
+      [[], [        ], [Stone(2)]],
+      [[], [Stone(1)], [        ]],
+    ])
+  })
+
+  test("can move stones left", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(1, 1)) // P1
+    game = play(game, Place(1, 2)) // P2
+    game = play(game, Move(1, 1, Left, [1])) // P1
+    
+    expect(game.board.squares).toEqual([
+      [[        ], [], [        ]],
+      [[Stone(1)], [], [Stone(2)]],
+      [[        ], [], [        ]],
+    ])
+  })
+
+  test("can move stones right", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(1, 1)) // P1
+    game = play(game, Place(1, 2)) // P2
+    game = play(game, Move(1, 1, Right, [1])) // P1
+    
+    expect(game.board.squares).toEqual([
+      [[], [], []],
+      [[], [], [Stone(2),Stone(1)]],
+      [[], [], []],
+    ])
+  })
+
+  test("cannot move out of bounds", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+    
+    expect(() => play(game, Move(0, 0, Up, [1]))).toThrow(
+      "Position out of bounds: rank -1, file 0 on a 3x3 board"
+    )
+  })
+
+  test("Rule 2.2.1: cannot move opponent's stones", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(0, 0, FlatStone)) // P1 turn 1
+    game = play(game, Place(1, 1, FlatStone)) // P2 turn 2
+    
+    // P1 tries to move P2's stone
+    expect(() => play(game, Move(1, 1, Right, [1]))).toThrow(
+      "Top stone at rank 1, file 1 belongs to Player 2, not Player 1"
+    )
+  })
+
+  test("Rule 2.2.1: can move multiple stones", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+    game = play(game, Place(1, 0)) // P1
+    game = play(game, Place(1, 1)) // P2
+    game = play(game, Move(1, 0, Up, [1])) // P1
+    game = play(game, Move(1, 1, Up, [1])) // P2
+    game = play(game, Move(0, 0, Right, [1, 1])) // P1
+
+    expect(game.board.squares).toEqual([
+      [[], [Stone(2),Stone(2),Stone(1)], [Stone(1)]],
+      [[], [                          ], [        ]],
+      [[], [                          ], [        ]],
+    ])
+  })
+
+  test("Rule 2.2.1: enforces carry limit according to board size", () => {
+    let game = createNewGame(3)
+
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+
+    for (let n=0; n<3; n++) {
+      game = play(game, Place(1, 0)) // P1
+      game = play(game, Place(1, 1)) // P2
+      game = play(game, Move(1, 0, Up, [1])) // P1
+      game = play(game, Move(1, 1, Up, [1])) // P2
+    }
+
+    expect(() => play(game, Move(0, 0, Right, [3])), "carry maximum of 3").not.toThrow()
+    
+    expect(() => play(game, Move(0, 0, Right, [4]))).toThrow(
+      "Cannot carry 4 stones (carry limit is 3)"
+    )
+  })
+
+  test("Rule 2.2.1: cannot carry more stones than are in a stack", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+
+    expect(() => play(game, Move(0, 0, Right, [1])), "pick up maximum").not.toThrow()
+    
+    expect(() => play(game, Move(0, 0, Right, [1, 1]))).toThrow(
+      "No stones left to pick up at rank 0, file 0"
+    )
+
+    expect(() => play(game, Move(0, 0, Right, [2]))).toThrow(
+      "No stones left to pick up at rank 0, file 0"
+    )
+  })
+
+  test("Rule 2.2.2: must drop at least one stone per square", () => {
+    let game = createNewGame(3)
+    
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+    game = play(game, Place(1, 0)) // P1
+    game = play(game, Place(1, 1)) // P2
+    game = play(game, Move(1, 0, Up, [1])) // P1
+    game = play(game, Move(1, 1, Up, [1])) // P2
+
+    expect(() => play(game, Move(0, 0, Right, [1]))).not.toThrow()
+    
+    expect(() => play(game, Move(0, 0, Right, [0, 1]))).toThrow(
+      "At least one stone must be dropped on each square along the path"
+    )
+  })
+
+  test("Rule 2.2.3: can flatten standing stone", () => {
+    let game = createNewGame(5)
+    
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+    game = play(game, Place(1, 0, CapStone)) // P1
+    game = play(game, Place(1, 1, StandingStone)) // P2
+    game = play(game, Move(1, 0, Right, [1])) // P1 flattens P2
+
+    expect(game.board.squares).toEqual([
+      [[Stone(1)], [Stone(2)                   ], [], [], []],
+      [[        ], [Stone(2),Stone(1, CapStone)], [], [], []],
+      [[        ], [                           ], [], [], []],
+      [[        ], [                           ], [], [], []],
+      [[        ], [                           ], [], [], []],
+    ])
+  })
+
+  test("Rule 2.2.3: capstone must be alone when flattening", () => {
+    let game = createNewGame(5)
+    
+    game = play(game, Place(0, 0)) // P1
+    game = play(game, Place(0, 1)) // P2
+    game = play(game, Place(1, 0, CapStone)) // P1
+    game = play(game, Place(1, 1)) // P2
+    game = play(game, Move(1, 0, Up, [1])) // P1 moves capstone onto own stone
+    game = play(game, Place(1, 0, StandingStone)) // P2
+
+    expect(() => play(game, Move(0, 0, Right, [1, 1]))).not.toThrow()
+    expect(() => play(game, Move(0, 0, Right, [2]))).not.toThrow()
+
+    expect(() => play(game, Move(0, 0, Down, [2]))).toThrow(
+      "Stones may only be dropped on flat stones and empty squares"
+    )
+  })
 })
--- src/model/play.ts
+++ src/model/play.ts
@@ -1,53 +1,129 @@
-import { updateBoard } from "./board";
-import { Game } from "./game";
+import { updateBoard, Position, Square } from "./board";
+import { Game, Reserve } from "./game";
 import { opponent } from "./players";
-import { Stone } from "./stones";
-import { Turn } from "./turns";
+import { Stone, CapStone, StandingStone, FlatStone, StoneType } from "./stones";
+import { Turn, Place, Move, Direction, Up, Down, Left, Right } from "./turns";
 
 /**
  * Given a Game state and a Turn, produces the resulting Game state.
  */
 export function play(game: Game, turn: Turn): Game {
-  const error = verifyPlay(game, turn)
-
-  if (error) {
-    throw new Error(`invalid turn for this game: ${error}`)
+  if (game.turn <= 2) {
+    if (turn.type === "move" || turn.stone !== FlatStone) {
+      throw new Error(`Turn ${game.turn} must place a flat stone`);
+    }
   }
 
   if (turn.type === "place") {
+    checkBounds(turn.position.rank, turn.position.file, game.board.size)
+
+    if (game.board.squares[turn.position.rank][turn.position.file].length > 0) {
+      throw new Error(`Stones can only be placed in empty squares`)
+    }
+
+    const stoneType = turn.stone === CapStone ? 'capstones' : 'stones'
+
+    if (game.reserve[game.player][stoneType] < 1) {
+      throw new Error(`Player ${game.player} does not have ${stoneType} in reserve`)
+    }
+
     return {
       player: opponent(game.player),
       turn: game.turn + 1,
       board: updateBoard(game.board, (square, rank, file) =>
         (turn.position.rank === rank) && (turn.position.file === file)
-          ? [Stone(game.player, turn.stone)]
+          ? [...square, Stone(game.player, turn.stone)]
           : square),
       reserve: {
         1: {
-          stones: game.reserve[1].stones - (game.player === 1 ? 1 : 0),
-          capstones: game.reserve[1].capstones
+          stones: game.reserve[1].stones - (game.player === 1 && turn.stone !== CapStone ? 1 : 0),
+          capstones: game.reserve[1].capstones - (game.player === 1 && turn.stone === CapStone ? 1 : 0)
         },
         2: {
-          stones: game.reserve[2].stones - (game.player === 2 ? 1 : 0),
-          capstones: game.reserve[2].capstones
+          stones: game.reserve[2].stones - (game.player === 2 && turn.stone !== CapStone ? 1 : 0),
+          capstones: game.reserve[2].capstones - (game.player === 2 && turn.stone === CapStone ? 1 : 0)
         }
       },
     }
   }
 
   if (turn.type === "move") {
-    // TODO
+    const board = updateBoard(game.board, square => square) // copy
+
+    let { rank, file } = turn.fromPosition
+
+    checkBounds(rank, file, game.board.size)
+
+    const topStone = game.board.squares[rank][file].at(-1)
+
+    if (!topStone) {
+      throw new Error(`No stones available to pick up at rank ${rank}, file ${file}`)
+    } else if (topStone.player !== game.player) {
+      throw new Error(`Top stone at rank ${rank}, file ${file} belongs to Player ${topStone.player}, not Player ${game.player}`)
+    }
+
+    const hand: Stone[] = []
+
+    for (const dropcount of turn.dropcounts) {
+      if (dropcount < 1) {
+        throw new Error(`At least one stone must be dropped on each square along the path (Rule 2.2.2)`)
+      }
+
+      for (let n=0; n<dropcount; n++) {
+        const stone = board.squares[rank][file].pop() // pick up
+
+        if (!stone) {
+          throw new Error(`No stones left to pick up at rank ${rank}, file ${file}`)
+        }
+
+        hand.push(stone) 
+      }
+    }
+
+    if (hand.length > board.size) {
+      throw new Error(`Cannot carry ${hand.length} stones (carry limit is ${board.size})`)
+    }
+
+    for (const dropcount of turn.dropcounts) {
+      switch (turn.direction) {
+        case Up: rank -= 1; break;
+        case Down: rank += 1; break;
+        case Left: file -= 1; break;
+        case Right: file += 1; break;
+        default:
+          throw new Error(`Invalid direction: ${turn.direction}`)
+      }
+
+      checkBounds(rank, file, game.board.size)
+
+      for (let n=0; n<dropcount; n++) {
+        const dropped = hand.pop()!
+
+        if (dropped.type !== CapStone && board.squares[rank][file].length > 0 && board.squares[rank][file].at(-1)!.type !== FlatStone) {
+          throw new Error(`Stones may only be dropped on flat stones and empty squares`)
+        }
+
+        if (dropped.type === CapStone && board.squares[rank][file].length > 0) {
+          board.squares[rank][file].at(-1)!.type = FlatStone // flatten
+        }
+
+        board.squares[rank][file].push(dropped)
+      }
+    }
+
+    return {
+      player: opponent(game.player),
+      turn: game.turn + 1,
+      board,
+      reserve: game.reserve,
+    };
   }
 
-  throw new Error(`Unsupported turn type: ${turn.type}`)
+  throw new Error(`Unsupported turn type: ${(turn as any).type}`)
 }
 
-/**
- * Given a Game state and a Turn, verify that the Turn is valid
- */
-export function verifyPlay(game: Game, turn: Turn): string | null {
-  // TODO verify all game rules
-  //      return a descriptive error message (or null if the turn is valid)
-  //      for each condition, reference the rule numbers in `tak-rules.md`
-  return null
+function checkBounds(rank: number, file: number, size: number): void {
+  if (rank < 0 || rank > size - 1 || file < 0 || file > size - 1) {
+    throw new Error(`Position out of bounds: rank ${rank}, file ${file} on a ${size}x${size} board`)
+  }
 }
